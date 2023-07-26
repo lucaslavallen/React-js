baf53a12568fd40c8746498e9add27be6f27dd3b
lucas <luquilavallen@gmail.com>
2023-07-26 09:27:27 -0300

    update folder

---
+++ 00-twitter-card/src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { App } from './App.jsx'
+import './index.css'
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+
+// Creando mi primer componente
+
+//craeando twitter Card
+
+// renderizado de la aplicacion
+root.render (
+    <App />
+)

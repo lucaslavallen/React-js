81774bc5d94e2ad906998f787a6b11db59fd3972
lucas <luquilavallen@gmail.com>
2023-07-26 09:16:56 -0300

    credno twitterCard

--- 00-Hola-mundo/src/App.jsx
+++ 00-Hola-mundo/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
+import { TwitterFollowCard } from "./TwitterFollowCard"
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App () {
+  const fromatUserName = (userName) => `@${userName}`
+  return(
+    <section className="App">
+    
+    <TwitterFollowCard 
+     fromatUserName={fromatUserName}
+     isFollowing userName="luqilavllen" 
+     name="lucas lavallen" />
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <TwitterFollowCard
+    fromatUserName={fromatUserName}
+    isFollowing={false}
+    userName="roman"
+    name=" rojas"/>
+
+    <TwitterFollowCard      
+    fromatUserName={fromatUserName}
+    isFollowing
+    userName="fede"
+    name="bojas"/>
+    </section>
   )
+  
 }
 
-export default App
--- 00-Hola-mundo/src/main.jsx
+++ 00-Hola-mundo/src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { App } from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+
+// Creando mi primer componente
+
+//craeando twitter Card
+
+// renderizado de la aplicacion
+root.render (
     <App />
-  </React.StrictMode>,
 )
